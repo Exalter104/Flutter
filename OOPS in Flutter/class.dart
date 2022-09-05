b327e871e821fc356bc34377b0588fc9a8c2e877
Muhammad Sheraz Khilji <100720842+Exalter104@users.noreply.github.com>
2022-09-05 23:11:32 +0000

    OOPs Concept

---
+++ OOPS in Flutter/class.dart
@@ -0,0 +1,11 @@
+class Sheraz(){
+int std1=20;
+int std2=20;
+int std3=20;
+
+int sum()
+{
+  return std1+std2+std3;
+}
+  
+}
---
+++ OOPS in Flutter/main.dart
@@ -0,0 +1,6 @@
+import 'class.dart';
+void main(){
+Sheraz she=Sheraz();
+she.sum();
+print("The sum is ${she.sum()}");
+}
